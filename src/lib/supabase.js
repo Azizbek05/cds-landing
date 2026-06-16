1045a050c5d832d2dc9e126cf39980cc97952271
Azizbek05 <azizamindjanov14@gmail.com>
2026-06-16 18:18:17 +0500

    Fix supabase key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://tytdhdddnfitrenovsob.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5dGRoZGRkbmZpdHJlbm92c29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5ODI0NTgsImV4cCI6MjA2NTU1ODQ1OH0.sb_publishable_195vYF0dQyHHXi5q2v9bvQ_WRD4KWtF'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5dGRoZGRkbmZpdHJlbm92c29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MDgxOTAsImV4cCI6MjA5NzA4NDE5MH0.MaPln0rmAnyBmwaK7sEvlIIYVwbEiMqBMxXh6VDc6og'
 )
